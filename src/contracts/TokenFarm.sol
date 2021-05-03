a94d63c0a0f1973f8eb7ca53919cdbc9d31b120a
Sum <sum@Sums-MacBook-Pro.local>
2021-05-03 23:11:53 +0800

    contract: transfer all DAI to TokenFarm & give 100 DAI to an account

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,6 +1,16 @@
 pragma solidity >=0.6.0 <=0.8.3;
 
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm {
   string public name = "Dapp Token Farm";
+  DappToken public dappToken;
+  DaiToken public daiToken;
+
+  constructor(DappToken _dappToken, DaiToken _daiToken) public {
+    dappToken = _dappToken;
+    daiToken = _daiToken;
+  }
   
 }
